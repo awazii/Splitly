30446b35a1e43facc6cd5e473d1034973431a947
Awazii <115639025+awazii@users.noreply.github.com>
2026-04-21 16:00:36 +0500

    Make Transactions for Friends in the group

--- src/Components/Groups/GroupDetail/fgroupdetails/Fgoverview.jsx
+++ src/Components/Groups/GroupDetail/fgroupdetails/Fgoverview.jsx
@@ -1,31 +1,36 @@
 import React from 'react'
 import { FaCoins } from "react-icons/fa";
 import { IoTicket } from "react-icons/io5";
-export const Fgoverview = ({Currentfriend}) => {
-     const overview = [
-            {
-                label: "Total Spendings",
-                value: Currentfriend.spendings,
-               gradient: "linear-gradient(135deg, #FF6B6B 0%, #FF8E53 50%, #FFA726 100%)",
-                svg: <FaCoins className='size-7 text-white' />
-            },
-            {
-                label: "Total Share",
-                value: Currentfriend.totalShare,
-               gradient: "linear-gradient(135deg, #C471F5 0%, #FA71CD 100%)",
-                svg: <IoTicket className='size-8 text-white' />
-            }]
-  return (
-    <div className='w-full h-full flex p-4'>
-         {overview.map((card, index) => (
+import { FriendGroupSpendings } from '../../../../store/ExpenseSlice'
+import { useSelector } from 'react-redux';
+import { useParams } from 'react-router-dom';
+export const Fgoverview = ({ Currentfriend }) => {
+    const { Groupid } = useParams()
+    const Currentfriendspending = useSelector(state => FriendGroupSpendings(state, Groupid, Currentfriend.id))
+    const overview = [
+        {
+            label: "Total Spendings",
+            value: (Currentfriendspending?.spent ?? 0),
+            gradient: "linear-gradient(135deg, #FF6B6B 0%, #FF8E53 50%, #FFA726 100%)",
+            svg: <FaCoins className='size-7 text-white' />
+        },
+        {
+            label: "Total Share",
+            value: (Currentfriendspending?.share ?? 0),
+            gradient: "linear-gradient(135deg, #C471F5 0%, #FA71CD 100%)",
+            svg: <IoTicket className='size-8 text-white' />
+        }]
+    return (
+        <div className='w-full h-full flex p-4'>
+            {overview.map((card, index) => (
                 <div key={index} className={`${card.label}  border-b-light center-flex w-full last:border-l-1 gap-2 mr-2 `}>
-                    {card.gradient? <div className="logo  size-18 rounded-full center-flex" style={{ background: card.gradient }}>{card.svg}</div> : <div className={`logo  size-18 rounded-full center-flex ${Currentfriend.balancebgClass}`}>{card.svg}</div> }
+                    <div className="logo  size-18 rounded-full center-flex" style={{ background: card.gradient }}>{card.svg}</div>
                     <div className="info w-fit h-2/3 center-flex flex-col">
                         <div className="title  font-semibold"> {card.label}</div>
-                        <div className={`description font-semibold  ${card.label==="Net Balance"? Currentfriend.balancetextClass:"text-text-secondary"}`}>{`Rs. ${card.value.toLocaleString()}${card.label==="Net Balance" && card.value===0 ?" (Settled)":"" }`}</div>
+                        <p className={`description font-semibold text-text-secondary `}>{`Rs. ${card.value.toLocaleString()}`}</p>
                     </div>
                 </div>
             ))}
-    </div>
-  )
+        </div>
+    )
 }
--- src/Components/Groups/GroupDetail/fgroupdetails/Transactiondetailsfg.jsx
+++ src/Components/Groups/GroupDetail/fgroupdetails/Transactiondetailsfg.jsx
@@ -4,8 +4,13 @@ import { MdPayments } from "react-icons/md";
 import { IoTicket } from "react-icons/io5";
 import { FaMoneyBillTransfer } from "react-icons/fa6";
 import { IoMdPerson } from "react-icons/io";
-export const Transactiondetailsfg = ({ isdetailopen, setisdetailopen }) => {
-    const currentTransaction = transactions.find(trans => trans.id === isdetailopen.id);
+import { categories } from '../../../Expenses/Expenses';
+import { indicators } from '../../../friends/Friendslist';
+import { Memberdetails } from '../../../../utils/Memberdetails';
+export const Transactiondetailsfg = ({ isdetailopen, setisdetailopen, Currentfriend }) => {
+    const currentTransaction = isdetailopen.trans
+    const Friend = currentTransaction.Members.find(f => f.id === Currentfriend.id)
+    const netbalance = Number(Friend.spent || 0) - Number(Friend.share || 0)
     return (
         <div className='transaction-details h-[94%] overflow-auto space-'>
             <div className="header center-flex w-fit gap-1 text-gray-800">
@@ -16,21 +21,21 @@ export const Transactiondetailsfg = ({ isdetailopen, setisdetailopen }) => {
             </div>
             <div className="transaction-info w-[50%] mx-auto my-2 py-4 bg-white rounded-xl h-fit shadow-md center-flex flex-col">
                 <div className="expense-info center-flex gap-2 flex-col">
-                    <div className="expense-logo size-15 rounded-full  center-flex shadow-md" style={{ background: currentTransaction.category.gradient }}>
-                        {currentTransaction.category.icon}
+                    <div className="expense-logo size-15 rounded-full  center-flex shadow-md" style={{ background: categories[currentTransaction.Category]?.gradient }}>
+                        {categories[currentTransaction.Category]?.icon}
                     </div>
-                    <p className='expense-name'>{currentTransaction.title}</p>
+                    <p className='expense-name'>{currentTransaction.Name}</p>
                 </div>
                 <div className="Total-particpants text-text-secondary center-flex gap-1 mb-1">
                     <IoMdPerson className='size-4' />
-                    <h3 className=' text-sm '>participants: <span className=' font-semibold'> {currentTransaction.details.split.length}
+                    <h3 className=' text-sm '>participants: <span className=' font-semibold'> {currentTransaction.Members.length}
                     </span> </h3>
                 </div>
                 <div className="expense-amount">
-                    <h2 className={`text-3xl font-semibold center-flex gap-1 `}>Rs.{Math.abs(currentTransaction.details.total).toLocaleString()} <span className='text-[13px]'>{`(Total expense)`}</span></h2>
+                    <h2 className={`text-3xl font-semibold center-flex gap-1 `}>Rs.{Math.abs(currentTransaction.totalAmount).toLocaleString()} <span className='text-[13px]'>{`(Total expense)`}</span></h2>
                 </div>
                 <div className="expense-date text-text-secondary text-sm">
-                    <p>{currentTransaction.date} <span>•</span> <span>{currentTransaction.time}</span></p>
+                    <p>{currentTransaction.createdDate}</p>
                 </div>
             </div>
             <div className="payment-details mt-2">
@@ -39,17 +44,18 @@ export const Transactiondetailsfg = ({ isdetailopen, setisdetailopen }) => {
                     <MdPayments className='size-5' />
                 </div>
                 <div className="paidby-list my-2 space-y-2 px-3 h-fit bg-white rounded-xl shadow-md w-[70%] mx-auto overflow-y-auto  py-2 ">
-                    {currentTransaction.details.paidBy.map((person, index) => {
+                    {currentTransaction.Members.map((member, index) => {
+                        if (member.spent == 0) return
                         return (
                             <div key={index} className="paidby-item flex justify-between items-center border-b border-b-light  pb-2 last:border-b-0">
                                 <div className="about center-flex gap-3">
                                     <div className="logo size-10 rounded-full border-b-light shadow-md border overflow-hidden center-flex">
-                                        <img src={Friends.find(friend => friend.id === person.userId)?.profilePic} alt="person-logo" className='Img-c border-none' />
+                                        <img src={Memberdetails(member.id)?.Image} alt="person-logo" className='Img-c border-none' />
                                     </div>
-                                    <p className=''>{`${person.name} paid`}</p>
+                                    <p className=''>{`${Memberdetails(member.id)?.Name}`}</p>
                                 </div>
                                 <div className="amount text-lg font-semibold text-text-primary">
-                                    Rs.{person.amount.toLocaleString()}
+                                    Rs.{Number(member.spent).toLocaleString()}
                                 </div>
                             </div>
                         )
@@ -63,24 +69,25 @@ export const Transactiondetailsfg = ({ isdetailopen, setisdetailopen }) => {
                 </div>
                 <div className="split-list my-2 space-y-2">
                     <div className="split-method">
-                        <h4 className='text-md font-semibold text-text-secondary center-flex gap-1'>Split Method: <span className='text-text-primary'>{currentTransaction.details.splitMethod}</span></h4>
+                        <h4 className='text-md font-semibold text-text-secondary center-flex gap-1'>Split Method: <span className='text-text-primary'>{currentTransaction.splitMethod}</span></h4>
                     </div>
-                    <div className="split-items mt-2 space-y-2 px-3 h-40  w-[70%] mx-auto overflow-y-auto ">
-                    {currentTransaction.details.split.map((person, index) => {
-                        return (
-                            <div key={index} className="split-item  bg-white p-2 rounded-lg shadow-md flex justify-between items-center">
-                                <div className="about center-flex gap-3">
-                                    <div className="logo size-10 rounded-full border-b-light shadow-md border overflow-hidden center-flex">
-                                        <img src={Friends.find(friend => friend.id === person.userId)?.profilePic} alt="person-logo" className='Img-c border-none' />
+                    <div className="split-items mt-2 space-y-2 px-3 max-h-40  w-[70%] mx-auto overflow-y-auto ">
+                        {currentTransaction.Members.map((member, index) => {
+                            if (member.share == 0) return
+                            return (
+                                <div key={index} className="paidby-item flex justify-between items-center pb-2 shadow rounded-md bg-white p-2">
+                                    <div className="about center-flex gap-3">
+                                        <div className="logo size-10 rounded-full border-b-light shadow-md border overflow-hidden center-flex">
+                                            <img src={Memberdetails(member.id)?.Image} alt="person-logo" className='Img-c border-none' />
+                                        </div>
+                                        <p className=''>{`${Memberdetails(member.id)?.Name}`}</p>
                                     </div>
-                                    <p className=''>{`${person.name + "'s share"}`}</p>
+                                    <div className="amount text-lg font-semibold text-text-primary">
+                                        Rs.{Number(member.share).toLocaleString()}
+                                    </div>
                                 </div>
-                                <div className="amount text-lg font-semibold text-text-primary">
-                                    Rs.{person.amount.toLocaleString()} {person.percent && <span className='text-sm text-text-secondary'>({person.percent}%)</span>}
-                                </div>
-                            </div>
-                        )
-                    })}
+                            )
+                        })}
                     </div>
                 </div>
             </div>
@@ -89,19 +96,19 @@ export const Transactiondetailsfg = ({ isdetailopen, setisdetailopen }) => {
                     <h3 className='text-xl font-semibold'>Settelemnts</h3>
                     <FaMoneyBillTransfer className='size-5' />
                 </div>
-                <div className={`settelemnts-list mt-2 space-y-2 ${currentTransaction.details.settlements.length!==0 ? "h-30" : "h-fit"}  w-[70%] mx-auto overflow-y-auto`}>
-                    {currentTransaction.details.settlements.length!==0
+                <div className={`settelemnts-list mt-2 space-y-2 ${(currentTransaction.Settlements.length !== 0 && netbalance !== 0) ? "h-30" : "h-fit"}  w-[70%] mx-auto overflow-y-auto`}>
+                    {(currentTransaction.Settlements.length !== 0 && netbalance !== 0)
                         ? <>
-                            {currentTransaction.details.settlements.map((settlement, index) => {
+                            {currentTransaction.Settlements.map((settlement, index) => {
                                 return (
                                     <div key={index} className="settlement-item  bg-white p-2 rounded-lg shadow-md flex justify-between items-center">
                                         <div className="about center-flex gap-3">
-                                            <div className={`logo size-9 center-flex  rounded-full ${settlement.to === "Awazii" ? "bg-green-600" : "bg-red-600"}`}>
+                                            <div className={`logo size-9 center-flex  rounded-full ${settlement.to === Currentfriend.id ? "bg-green-600" : "bg-red-600"}`}>
                                                 <FaMoneyBillTransfer className='size-5 text-white' />
                                             </div>
-                                            <p className=''>{`${settlement.from} owes ${settlement.to}`}</p>
+                                            <p className=''>{`${Memberdetails(settlement.from)?.Name} owes ${Memberdetails(settlement.to)?.Name}`}</p>
                                         </div>
-                                        <div className={`amount text-lg font-semibold ${settlement.to === "Awazii" ? "text-green-600" : "text-red-600"}`}>
+                                        <div className={`amount text-lg font-semibold ${settlement.to === Currentfriend.id ? "text-green-600" : "text-red-600"}`}>
                                             Rs.{settlement.amount.toLocaleString()}
                                         </div>
                                     </div>
